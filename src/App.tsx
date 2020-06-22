ed12f852e382e08578183a52433213a275b340e8
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-06-22 12:03:38 +0900

    完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
-import React, { useState, FormEvent } from 'react'
-
+import React, { useState, FormEvent, ChangeEvent } from 'react'
+import axios from 'axios'
 import M from 'materialize-css'
+
+const webhookURL: string = process.env.REACT_APP_SLACK_WEBHOOK || ''
 
 const App = () => {
   const [text, setText] = useState('')
   const [isSubmitting, setSubmitting] = useState(false)
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const input: string = e.target.value
+    setText(input)
+  }
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
+    console.log(webhookURL)
     setSubmitting(true)
-    window.M.toast({ html: '送信しました！', classes: 'green rounded' })
+    console.log(text)
+    const payload: any = {
+      channel: '#つぶやき',
+      username: 'tweet bot',
+      text: text,
+      icon_emoji: 'hatching_chick'
+    }
+
+    const config: any = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      }
+    }
+
+    axios
+      .post(webhookURL, payload, config)
+      .then(res => {
+        console.log(res)
+        window.M.toast({ html: '送信しました！', classes: 'green rounded' })
+        setText('')
+      })
+      .catch(e => {
+        window.M.toast({
+          html: `送信に失敗しました… ${e}`,
+          classes: 'red rounded'
+        })
+      })
     setSubmitting(false)
   }
 
@@ -20,29 +54,48 @@ const App = () => {
           <span className='brand-logo center'>&#x1f423;</span>
         </div>
       </nav>
+      <div style={{ height: '5em' }} />
       <div className='container'>
-        <form onSubmit={handleSubmit}>
-          <input type='text' />
-          <button
-            className='btn waves-effect waves-light indigo'
-            type='submit'
-            name='action'
-          >
-            送信
-            <i className='material-icons right'>send</i>
-          </button>
-          <div
-            className={`preloader-wrapper small ${isSubmitting && 'active'}`}
-          >
-            <div className='spinner-layer spinner-green-only'>
-              <div className='circle-clipper left'>
-                <div className='circle'></div>
-              </div>
-              <div className='gap-patch'>
-                <div className='circle'></div>
-              </div>
-              <div className='circle-clipper right'>
-                <div className='circle'></div>
+        <p className='grey-text small'>　つぶやきはこちらから</p>
+        <form onSubmit={(e: FormEvent) => e.preventDefault()}>
+          <div className='row'>
+            <div className='col s8'>
+              <input
+                type='text'
+                placeholder='いまどうしてる？'
+                value={text}
+                onChange={handleChange}
+              />
+            </div>
+            <div
+              className='col s4 valign-wrapper'
+              style={{ textAlign: 'center' }}
+            >
+              <button
+                className='btn waves-effect waves-light indigo'
+                type='button'
+                onClick={handleSubmit}
+                disabled={!text}
+              >
+                送信
+                <i className='material-icons right'>send</i>
+              </button>
+              <div
+                className={`preloader-wrapper small ${
+                  isSubmitting && 'active'
+                }`}
+              >
+                <div className='spinner-layer spinner-green-only'>
+                  <div className='circle-clipper left'>
+                    <div className='circle'></div>
+                  </div>
+                  <div className='gap-patch'>
+                    <div className='circle'></div>
+                  </div>
+                  <div className='circle-clipper right'>
+                    <div className='circle'></div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
